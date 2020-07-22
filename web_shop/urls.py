a3c32be869925a9743aa1146d3e6a994b8fa557c
Valentyn Anzhurov <valentyn_anzhurov@epam.com>
2020-07-22 21:27:01 +0300

    master. Fix admin page doesn't appear

--- web_shop/urls.py
+++ web_shop/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="welcome.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
     path('product/', include('products.urls'))
 ]
