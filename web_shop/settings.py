4e7263c3daa165abafe8d191871bdf400215b118
Valentyn Anzhurov <valentyn_anzhurov@epam.com>
2020-07-22 17:41:35 +0300

    master. extract db params to os env variables

--- web_shop/settings.py
+++ web_shop/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'web_shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'web_shop',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
