32cca7498214d440d31d5709e6772cd7fa4a97f1
Valentyn Anzhurov <valentyn_anzhurov@epam.com>
2020-07-22 21:55:45 +0300

    static-css. Add styles to templates

--- web_shop/urls.py
+++ web_shop/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
-    path('product/', include('products.urls'))
+    path('products/', include('products.urls'))
 ]
